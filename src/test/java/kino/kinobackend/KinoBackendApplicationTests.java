0046e39395d8c6834e230151b6cd4272c76313fb
Telity <Nigu0001@Stud.kea.dk>
2025-02-28 13:21:44 +0100

    Initial

---
+++ src/test/java/kino/kinobackend/KinoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kino.kinobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
