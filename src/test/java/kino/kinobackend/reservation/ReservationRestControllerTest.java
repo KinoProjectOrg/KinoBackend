613eb463b01d2b506d8ce0b7ddfbf106e325c4f0
Telity <Nigu0001@Stud.kea.dk>
2025-03-03 19:40:24 +0100

    KIN-59 KIN-15 KIN-52
    Test on RestController 4/5 tested

--- src/test/java/kino/kinobackend/reservation/ReservationRestControllerTest.java
+++ src/test/java/kino/kinobackend/reservation/ReservationRestControllerTest.java
@@ -1,19 +1,25 @@
 package kino.kinobackend.reservation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.slf4j.MDC.get;
+
 
 @WebMvcTest(ReservationRestController.class)
 class ReservationRestControllerTest {
@@ -34,26 +40,62 @@ class ReservationRestControllerTest {
     }
 
     @Test
-    void getAll() {
+    void getAll() throws Exception {
 
         List<ReservationModel> reservations = new ArrayList<>();
         reservations.add(reservationModel);
 
         Mockito.when(reservationService.allReservations()).thenReturn(reservations);
 
-        mockMvc.perform(get("/reservation/get"))
+        mockMvc.perform(get("/reservation/get")
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.length()").value(1));
     }
 
     @Test
-    void getById() {
+    void getById() throws Exception {
+
+        Mockito.when(reservationService.findReservationById(1)).thenReturn(reservationModel);
+
+        mockMvc.perform(get("/reservation/get/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.reservation_id").value(1));
+
+
     }
 
     @Test
-    void create() {
+    void create() throws Exception {
+
+        Mockito.when(reservationService.createReservation(Mockito.any(ReservationModel.class)))
+                .thenReturn(reservationModel);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(reservationModel);
+
+        mockMvc.perform(post("/reservation/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.reservation_id").value(1));
+
     }
 
     @Test
-    void update() {
+    void update() throws Exception {
+
+        Mockito.when(reservationService.updateReservation(Mockito.any(ReservationModel.class)));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(reservationModel);
+
+        mockMvc.perform(put("/reservation/update/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.reservation_id").value(1));
     }
 
     @Test
