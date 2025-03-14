bebd675eec4c2f1844fee97ae456e2c7da7977d4
Jhockinn <Tobiln@hotmail.dk>
2025-03-14 12:56:35 +0100

    updated the database with better dummy daata

--- src/test/java/kino/kinobackend/movie/MovieControllerIntegrationTest.java
+++ src/test/java/kino/kinobackend/movie/MovieControllerIntegrationTest.java
@@ -9,6 +9,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,41 +34,89 @@ class MovieControllerIntegrationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+
     private MovieModel movie1;
     private MovieModel movie2;
+
+
 
     @BeforeEach
     void setUp() {
         movieRepository.deleteAll();
 
         movie1 = new MovieModel();
+        movie1.setId(1);
         movie1.setTitle("Movie1");
-        movie1.setGenreNames();
-        movie1.setMinAge(12);
-        movie1.
+        movie1 = movieRepository.save(movie1);
+
+        movie2 = new MovieModel();
+        movie2.setId(2);
+        movie2.setTitle("Movie2");
+        movie2 = movieRepository.save(movie2);
     }
 
     @Test
-    void getUpcomingMovies() {
+    void getUpcomingMoviesIntegrationTest() throws Exception {
+        List<MovieModel> futureMoviesTest = movieRepository.findAll();
+        mockMvc.perform(get("/movie/upcoming")
+        .contentType(MediaType.APPLICATION_JSON)
+                        .content(futureMoviesTest.toString()))
+                .andExpect(status().isNotFound());
     }
 
     @Test
-    void getMovies() {
+    void getMoviesIntegrationTest()  throws Exception {
+        mockMvc.perform(get("/movies/get")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(movieRepository.count()))
+                .andExpect(jsonPath("$[0].title").value(movie1.getTitle()))
+                .andExpect(jsonPath("$[1].title").value(movie2.getTitle()));
     }
 
     @Test
-    void getMovie() {
+    void getMovieIntegrationTest() throws Exception {
+        mockMvc.perform(get("/movies/get/" + movie1.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(movie1.getId()));
     }
 
     @Test
-    void createMovie() {
+    void createMovieIntegrationTest() throws Exception {
+        MovieModel newMovie = new MovieModel();
+        newMovie.setTitle("Lord of the Rings");
+
+        mockMvc.perform(post("/movies/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(newMovie)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").value(newMovie.getTitle()));
+
+        assertEquals(3, movieRepository.count());
     }
 
     @Test
-    void updateMovie() {
+    void updateMovieIntegrationTest() throws Exception {
+        MovieModel movieUpdate = new MovieModel();
+        movieUpdate.setId(movie1.getId());
+        movieUpdate.setTitle("Spiderman");
+
+        mockMvc.perform(put("/movies/filmoperator/update/" + movieUpdate.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(movieUpdate)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Spiderman"));
+        assertEquals(2, movieRepository.count());
     }
 
     @Test
-    void deleteMovie() {
+    void deleteMovieIntegrationTest() throws Exception {
+        mockMvc.perform(delete("/movies/filmoperator/delete/" + movie1.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        assertEquals(1, movieRepository.count());
+        assertFalse(movieRepository.existsById(movie1.getId()));
     }
 }
